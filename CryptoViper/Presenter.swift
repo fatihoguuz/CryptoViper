bf47876b71458d4f7e072bdd92114fea3891a768
Fatih Oguz <oguzfatih39@gmail.com>
2024-08-06 10:47:56 +0300

    VIPER app is finish

--- CryptoViper/Presenter.swift
+++ CryptoViper/Presenter.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+
+// Class
+// Protocol
+// Talks To -> Interactor, Router, View
+
+enum NetworkError : Error {
+    case NetworkFailed
+    case ParsingFailed 
+}
+
+protocol AnyPresenter {
+    
+    var router : AnyRouter? {get set}
+    var interactor : AnyInteractor? {get set}
+    var view : AnyView? {get set}
+    
+    func interactorDidDownloadCrypto(result:Result<[Crypto], Error>)
+}
+
+
+class CryptoPresenter: AnyPresenter {
+    var router: AnyRouter?
+    
+    var interactor: AnyInteractor?{
+        didSet{
+            interactor?.downloadCryptos()
+        }
+    }
+    
+    var view: AnyView?
+    
+    func interactorDidDownloadCrypto(result: Result<[Crypto], Error>) {
+        switch result {
+        case .success(let cryptos):
+            view?.update(with: cryptos)
+            
+        case .failure(_):
+            view?.update(with: "Try Again ...")        }
+        
+    }
+}
--- CryptoViper/Router.swift
+++ CryptoViper/Router.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+import UIKit
+
+// Class
+
+typealias EntryPoint = AnyView & UIViewController
+
+protocol AnyRouter {
+    var entry : EntryPoint? {get}
+    static func startExecution() -> AnyRouter
+}
+
+class CryptoRouter : AnyRouter {
+    var entry : EntryPoint?
+    
+    static func startExecution() -> AnyRouter {
+        let router = CryptoRouter()
+        
+        var view : AnyView = CryptoView()
+        var interactor : AnyInteractor = CryptoInteractor()
+        var presenter : AnyPresenter = CryptoPresenter()
+        
+        
+        view.presenter = presenter
+        presenter.view = view
+        presenter.interactor = interactor
+        presenter.router = router
+        interactor.presenter = presenter
+        
+        router.entry = view as? EntryPoint
+        
+    return router
+    }
+}
+
